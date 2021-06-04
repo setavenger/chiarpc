a30b4481beee320222cfae281d9de23395fa1c2f
Setavenger <setor.blagogee@gmx.de>
2021-06-04 11:04:55 +0200

    removed print statement

--- wallet.go
+++ wallet.go
@@ -16,7 +16,6 @@ func (c Client) SendTransaction(amount uint64, address string, fee uint64, walle
 
 	data := map[string]interface{}{"amount": amount, "address": address, "fee": fee, "wallet_id": walletId}
 	responseRaw, err := c.makeRPCCall(http.MethodPost, "send_transaction", WalletPort, data, nil)
-	log.Println(responseRaw)
 	if err != nil {
 		log.Println(err)
 	}
@@ -25,6 +24,5 @@ func (c Client) SendTransaction(amount uint64, address string, fee uint64, walle
 	if err != nil {
 		log.Println(err)
 	}
-
 	return respData, nil
 }
